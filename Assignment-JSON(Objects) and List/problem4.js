adda7585fbe641f1c68359bd7e1182d5fa00d4ad
Ayushmaan Verma <56289697+thatssoayushmaan@users.noreply.github.com>
2021-04-21 21:00:16 +0530

    Create problem4.js

---
+++ Assignment-JSON(Objects) and List/problem4.js
@@ -0,0 +1,8 @@
+var arr = ["GUVI", "I", "am", "a geek"];
+function transformFirstAndLast(arr) {
+    let newObject = {}
+    newObject[arr[0]] = arr[arr.length-1]
+    return newObject;
+}
+
+console.log(transformFirstAndLast(arr))
